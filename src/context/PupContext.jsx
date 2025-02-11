b0e32ad2539da151f975b0923afc6b1b5595a18a
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2025-02-10 16:41:16 -0800

    created PupProvider context component, imported into main jsx

--- src/context/PupContext.jsx
+++ src/context/PupContext.jsx
@@ -1,3 +1,13 @@
 import { createContext } from 'react';
 
+export const PupProvider = (props) => {
+  return (
+    <PupContext.Provider>
+      {props.children}
+    </PupContext.Provider>
+  )
+}
+
+
+
 export const PupContext = createContext();
